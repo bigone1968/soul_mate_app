aa862dd73fa8aa9cb682d12d07f18ca27d7c8b43
黄文端 <164400943@qq.com>
2026-05-30 22:22:13 +0800

    移除阿里云Maven镜像(云端构建502)，仅保留google和mavenCentral

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,14 +1,8 @@
 allprojects {
     repositories {
-        maven { setUrl("https://maven.aliyun.com/repository/public") }
-        maven { setUrl("https://maven.aliyun.com/repository/google") }
         google()
         mavenCentral()
     }
-}
-
-subprojects {
-    project.evaluationDependsOn(":app")
 }
 
 tasks.register<Delete>("clean") {
