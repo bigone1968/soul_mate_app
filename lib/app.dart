af11d998379a336d50cb06296df885465dceae59
bigone1968 <138571885@qq.com>
2026-06-01 01:31:05 +0800

    feat: integrate FlowerApp design - home/sleep/settings pages

--- lib/app.dart
+++ lib/app.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'theme/app_theme.dart';
 import 'providers/chat_provider.dart';
 import 'screens/splash_screen.dart';
 
-class SoulMateApp extends StatelessWidget {
+class FlowerApp extends StatelessWidget {
   final String apiKey;
   final String iflytekAppId;
   final String iflytekApiKey;
   final String iflytekApiSecret;
-  const SoulMateApp({
+
+  const FlowerApp({
     super.key,
     required this.apiKey,
     required this.iflytekAppId,
@@ -19,18 +19,37 @@ class SoulMateApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider<ChatProvider>(
-      create: (_) => ChatProvider(
-        apiKey: apiKey,
-        iflytekAppId: iflytekAppId,
-        iflytekApiKey: iflytekApiKey,
-        iflytekApiSecret: iflytekApiSecret,
-      ),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => ChatProvider(
+            apiKey: apiKey,
+            iflytekAppId: iflytekAppId,
+            iflytekApiKey: iflytekApiKey,
+            iflytekApiSecret: iflytekApiSecret,
+          ),
+        ),
+      ],
       child: MaterialApp(
         title: '彼岸花开',
-        theme: AppTheme.theme(),
-        home: const SplashScreen(),
         debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          scaffoldBackgroundColor: const Color(0xFF0A050E),
+          colorScheme: const ColorScheme.dark(
+            primary: Color(0xFFC0392B),
+            secondary: Color(0xFFE74C3C),
+          ),
+          appBarTheme: const AppBarTheme(backgroundColor: Colors.transparent, elevation: 0),
+          bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+            backgroundColor: Color(0xFF0D0508),
+            selectedItemColor: Color(0xFFC0392B),
+            unselectedItemColor: Color(0xFF8A7A6B),
+            type: BottomNavigationBarType.fixed,
+            elevation: 20,
+          ),
+        ),
+        home: const SplashScreen(),
       ),
     );
   }
